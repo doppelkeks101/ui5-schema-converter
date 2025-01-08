272d20b766165a475356a5039e59e3d7b39c2095
doppelkeks101 <doppelkeks101@web.de>
2025-01-08 15:39:52 +0100

    wip

--- main.py
+++ main.py
@@ -3,3 +3,19 @@
 # UI5 API examples
 # https://sapui5.hana.ondemand.com/test-resources/sap/ui/core/designtime/apiref/api.json
 # https://sapui5.hana.ondemand.com/test-resources/sap/m/designtime/apiref/api.json
+
+import requests
+import json
+import sys
+import xml.etree.ElementTree as ET
+from string import Template
+
+TEMPLATE = Template("https://sapui5.hana.ondemand.com/test-resources$package/designtime/apiref/api.json")
+
+api = TEMPLATE.substitute(package="/sap/m")
+print("API: " + api)
+
+response = requests.get(api)
+json = response.json()
+
+print(len(json["symbols"]))
