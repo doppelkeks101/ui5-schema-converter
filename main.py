483335bbe991c517007a3dfc558ccc99837a6482
doppelkeks101 <doppelkeks101@web.de>
2025-01-08 15:01:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Script that fetches SAPUI5 API metadata and converts it to XSD schema files
+# 
+# UI5 API examples
+# https://sapui5.hana.ondemand.com/test-resources/sap/ui/core/designtime/apiref/api.json
+# https://sapui5.hana.ondemand.com/test-resources/sap/m/designtime/apiref/api.json
